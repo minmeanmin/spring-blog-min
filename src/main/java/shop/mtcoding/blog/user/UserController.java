ca70435ee139718fd7b398828e5c2141938345e2
minmeanmin <wed1604@naver.com>
2024-02-06 18:55:59 +0900

    회원가입 구현하기

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,10 +1,23 @@
 package shop.mtcoding.blog.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO requestDTO){
+        System.out.println(requestDTO);
+
+        userRepository.save(requestDTO); // 모델에 위임하기
+        return "redirect:/loginForm";
+    }
 
     @GetMapping("/joinForm")
     public String joinForm() {
